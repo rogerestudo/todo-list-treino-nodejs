6bffd34eed38e0b182ff3a57572ec36d902d62e2
cezarfbc <cezarfbc@gmail.com>
2024-11-09 16:01:39 -0300

    reset project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TodoListModule } from './todo_list/todo_list.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TodoListModule, PrismaModule],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
